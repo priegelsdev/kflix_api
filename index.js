240276ef7809c5f3c95ef042fc5654e6be7c0eec
priegelsdev <priegelsdev@gmail.com>
2023-04-20 13:43:23 +0200

    add get requests and listen for req

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // import express
 const express = require('express')
+// set express to variable app
 const app = express()
 
-const http = require('http');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my book club!\n');
-}).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+// GET requests
+  // app.METHOD(PATH, HANDLER)
+  // app as instance of express()
+  // express constructs header with misc info
+app.get('/', (req, res) => {
+  res.send('Welcome to this project.')
+})
+
+// __dirname as module-specific variable providing path to current directory
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', {root: __dirname})
+})
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('App is listening on port 8080.')
+})
