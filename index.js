b7ff5044d7d143e447021be67adacaaa369f8f1c
priegelsdev <priegelsdev@gmail.com>
2023-04-20 13:54:54 +0200

    add morgan middleware and auto create and fill log.txt

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 const express = require('express')
 // import middleware Morgan to log info for requests
 const morgan = require('morgan')
+// import node modules fs and path
+const fs = require('fs')
+  path = require('path')
+
 // set express to variable app
 const app = express()
+// create write stream (in append mode)
+// 'log.txt' is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 // designating that morgan should be called with every req
   // no need for console.logging in each request
-app.use(morgan('common'))
+app.use(morgan('combined', {stream: accessLogStream}))
 
 // GET requests
   // app.METHOD(PATH, HANDLER)
