66988337f26ae15161c929466dd92cd6679d9e20
priegelsdev <priegelsdev@gmail.com>
2023-04-21 10:19:10 +0200

    install bodyparser and uuid

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express')
 // import middleware Morgan to log info for requests
 const morgan = require('morgan')
+// import middleware bodyparser to extract body portion of incoming request stream so it's easier to interface with
+const bodyParser = require('body-parser')
+// import uuid 
+const uuid = require('uuid')
 // import node modules fs and path
 const fs = require('fs')
   path = require('path')
@@ -17,6 +21,9 @@ app.use(express.static('public'))
 // designating that morgan should be called with every req
   // no need for console.logging in each request
 app.use(morgan('combined', {stream: accessLogStream}))
+app.use(bodyParser.json())
+
+// TODO: DATA HERE
 
 // GET requests
   // app.METHOD(PATH, HANDLER)
