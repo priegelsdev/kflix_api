9ff0a5cbd331ec5b917a7cc2a770916e2e2b416f
priegelsdev <priegelsdev@gmail.com>
2023-04-21 12:59:03 +0200

    import jwt and passport

---
+++ auth.js
@@ -0,0 +1,7 @@
+const jwtSecret = 'your_jwt_secret' // same as used in JWTStrategy
+
+// import jwt and passport
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+
+require('./passport') // local passport file
