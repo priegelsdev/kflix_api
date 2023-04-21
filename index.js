09acd4730fa882d2baa64c6f4bea67b24251335d
priegelsdev <priegelsdev@gmail.com>
2023-04-21 10:21:32 +0200

    add data

--- index.js
+++ index.js
@@ -24,6 +24,117 @@ app.use(morgan('combined', {stream: accessLogStream}))
 app.use(bodyParser.json())
 
 // TODO: DATA HERE
+let users = [
+  {
+    id: '1',
+    name: 'user1',
+    username: 'user1',
+    password: 'password',
+    email: "user1@gmail.com"
+  }
+]
+
+let movies = [
+  {
+    'id': '1',
+    'title': 'Oldboy',
+    'director': 'Park Chan-Wook',
+    'genre': [
+      'Thriller',
+      'Drama'
+    ],
+    'release_date': '2003'
+  },
+  {
+    'id': '2',
+    'title': 'Parasite',
+    'director': 'Bong Joon-ho',
+    'genre': [
+      'Drama',
+      'Comedy',
+      'Thriller'
+    ],
+    'release_date': '2019'
+  },
+  {
+    'id': '3',
+    'title': 'The Handmaiden',
+    'director': 'Park Chan-Wook',
+    'genre': [
+      'Romance',
+      'Drama'
+    ],
+    'release_date': '2016'
+  },
+  {
+    'id': '4',
+    'title': 'I Saw the Devil',
+    'director': 'Kim Jee-woon',
+    'genre': [
+      'Thriller',
+      'Action'
+    ],
+    'release_date': '2010'
+  },
+  {
+    'id': '5',
+    'title': 'Mother',
+    'director': 'Bong Joon-ho',
+    'genre': 'Mystery',
+    'release_date': '2009'
+  },
+  {
+    'id': '6',
+    'title': 'Joint Security Area',
+    'director': 'Park Chan-Wook',
+    'genre': [
+      'Mystery',
+      'Drama'
+    ],
+    'release_date': '2010'
+  },
+  {
+    'id': '7',
+    'title': 'A Bittersweet Life',
+    'director': 'Kim Jee-woon',
+    'genre': [
+      'Action',
+      'Drama'
+    ],
+    'release_date': '2005'
+  },
+  {
+    'id': '8',
+    'title': 'Memories of Murder',
+    'director': 'Bong Joon-ho',
+    'genre': [
+      'Crime',
+      'Thriller'
+    ],
+    'release_date': '2003'
+  },
+  {
+   'id': '9',
+   'title': 'The Chaser',
+   'director': 'Na Hong-jin',
+   'genre': [
+     'Thriller',
+     'Action'
+   ],
+   'release_date': '2008'
+  },
+  {
+    'id': '10',
+    'title': 'A Tale of Two Sisters',
+    'director': 'Kim Jee-woon',
+    'genre': [
+      'Horror',
+      'Thriller'
+    ],
+    'release_date': '2003'
+  }
+];
+
 
 // GET requests
   // app.METHOD(PATH, HANDLER)
