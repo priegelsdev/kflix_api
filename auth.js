d7671e9f08cc2b0a22976c0392d1f9eae13f454c
priegelsdev <priegelsdev@gmail.com>
2023-04-21 13:02:12 +0200

    write logic to generate JWT token

--- auth.js
+++ auth.js
@@ -5,3 +5,13 @@ const jwt = require('jsonwebtoken')
 const passport = require('passport')
 
 require('./passport') // local passport file
+
+// function to generate the JWT Token
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // username to be encoded in JWT
+    expiresIn: '7d', // specifies that token will expire in 7 days
+    algorithm: 'HS256' // algorithm used to sign or encode values of JWT
+  })
+}
+
