a012c6ec44bc7d2ab2df5b410febcb330deb449f
priegelsdev <priegelsdev@gmail.com>
2023-04-21 12:30:40 +0200

    create and export models

--- models.js
+++ models.js
@@ -5,6 +5,21 @@
   // model specifies what data to store and how to store it --> BUSINESS LOGIC
 const mongoose = require('mongoose')
 
+// SCHEMA DOC
+  /*
+    Keys correspond to fields
+    Values correspond to data types
+  
+    let <collectionSchema> = mongoose.<Schema>({
+      Key: {Value},
+      Key: {Value},
+      Key: {
+        Key: {Value},
+        Key: {Value},
+      }
+    })
+  */
+
 // define movieSchema
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
@@ -13,6 +28,7 @@ let movieSchema = mongoose.Schema({
     Name: String,
     Description: String
   },
+  // director key refers to document in collection
   Director: { type:mongoose.Schema.Types.ObjectId, ref: 'Director'},
   ImagePath: String,
   Featured: Boolean
@@ -39,3 +55,15 @@ let genreSchema = mongoose.Schema({
   Description: {type: String, required: true}
 })
 
+// create models that use defined schemas
+// creates collections with names in lowercase and pluralized (Movie => db.movies)
+let Movie = mongoose.model('Movie', movieSchema)
+let User = mongoose.model('User', userSchema)
+let Director = mongoose.model('Director', directorSchema)
+let Genre = mongoose.model('Genre', genreSchema)
+
+// exporting models, so API endpoints can query MongoDB
+module.exports.Movie = Movie
+module.exports.User = User
+module.exports.Director = Director
+module.exports.Genre = Genre
